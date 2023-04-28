172f08f93faadfc6d5beffeeb094efbdd6abb46b
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-27 21:39:42 -0700

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Module for solving the "Lockboxes" interview question.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked given a list of boxes, where each
+    box contains keys to other boxes, and the first box is initially unlocked.
+    A key with the same number as a box opens that box. Returns True if all
+    boxes can be opened, else returns False.
+    """
+
+
+n = len(boxes)
+visited = [False] * n
+visited[0] = True
+stack = [0]
+while stack:
+    box = stack.pop()
+    for key in boxes[box]:
+        if key < n and not visited[key]:
+            visited[key] = True
+            stack.append(key)
+return all(visited)
