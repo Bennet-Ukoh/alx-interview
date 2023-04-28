b34b31bd503f8cb6cb34e36257f6b3d7879ad90c
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-27 21:50:38 -0700

    code fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,16 +12,14 @@ def canUnlockAll(boxes):
     A key with the same number as a box opens that box. Returns True if all
     boxes can be opened, else returns False.
     """
-
-
-n = len(boxes)
-visited = [False] * n
-visited[0] = True
-stack = [0]
-while stack:
-    box = stack.pop()
-    for key in boxes[box]:
-        if key < n and not visited[key]:
-            visited[key] = True
-            stack.append(key)
-return all(visited)
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+    return all(visited)
