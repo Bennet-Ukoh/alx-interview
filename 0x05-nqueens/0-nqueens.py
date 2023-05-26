c4aaac177f656672ae2bcc729f95c73ba2a7f92a
Bennet-Ukoh <bennetukoh@gmail.com>
2023-05-26 04:59:33 +0000

    correction updeta

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -18,7 +18,7 @@ def is_safe(board, row, col):
             board[i] == col
             or board[i] - i == col - row
             or board[i] + i == col + row
-):
+        ):
     return False
     return True
 
