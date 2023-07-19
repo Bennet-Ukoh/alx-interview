b17b1a4b5cb0c3c1dc788ac343ae2ac2462217a4
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-19 10:44:11 +0000

    code refactored

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,23 @@
 #!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmId = process.argv[2];
 
-const request = require('request');
+async function main(id) {
+  try {
+    const filmEndpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+    let filmResponse = await request(filmEndpoint);
+    const filmData = JSON.parse(filmResponse.body);
+    const characters = filmData.characters;
 
-// Get the Movie ID from command-line argument
-const movieId = process.argv[2];
-
-// Make the API request to fetch the movie details
-request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status:', response.statusCode);
-  } else {
-    // Parse the response body as JSON
-    const movieData = JSON.parse(body);
-
-    // Extract the character URLs from the movie data
-    const characterURLs = movieData.characters;
-
-    // Make separate API requests for each character to fetch their details
-    characterURLs.forEach((characterURL) => {
-      request(characterURL, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error:', charError);
-        } else if (charResponse.statusCode !== 200) {
-          console.error('Status:', charResponse.statusCode);
-        } else {
-          // Parse the character data and print the name
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
-      });
-    });
+    for (const characterEndpoint of characters) {
+      let characterResponse = await request(characterEndpoint);
+      const characterData = JSON.parse(characterResponse.body);
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
   }
-});
+}
+
+main(filmId);
